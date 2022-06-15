ba2b6977426b9c4931fde6ccfa0b43747899c982
Mathis H <mathishumbert71@gmail.com>
2022-06-15 15:23:26 +0200

    fixed post board

--- pages/api/board/index.js
+++ pages/api/board/index.js
@@ -25,3 +25,20 @@ export default async function handler(req, res) {
     res.status(200).json({ message: 'Board added to collection' });
   }
 }
+
+// board: {
+//   name: '',
+//   _id: 'id',
+//   columns: ['', '']
+// }
+
+// task: {
+//   _id: 'id'
+//   board_id: 'id'
+//   status: 'colunm name',
+//   description: '',
+//   title: '',
+//   subtasks: [
+
+//   ]
+// }
